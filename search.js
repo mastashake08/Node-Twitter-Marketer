ea1117883eef0119e12f3db54cabb36de65879aa
Jyrone Parker <jyrone.parker@gmail.com>
2016-04-03 16:45:23 -0400

    added tweets.csv

--- search.js
+++ search.js
@@ -1,4 +1,5 @@
 var Twit = require('twit')
+var LineByLineReader = require('line-by-line');
 require('dotenv').config();
 
 var locations = [['-115.414625','36.129623','-115.062072','36.380623'],['36.49','-89.57','39.15','-81.97']];
@@ -9,7 +10,32 @@ var T = new Twit({
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
-var LineByLineReader = require('line-by-line');
+var tweets = [
+  ' Please, check out my video and R/T to people who want to save the kids!! https://youtu.be/XoKqCQDNmmY #Freshwadda',
+  ' Trying to reach all of those who care about our next generation,  R/T and share please: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' Music Video response to the presidential campaign, please R/T: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' My music video "For The People!!"!! Please R/T: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' My music video "WARNING", a message to the kids: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' Please review & R/T my music video talking about lessons we must pass on: https://youtu.be/XoKqCQDNmmY  #Freshwadda'
+]
+var t = new LineByLineReader('tweets.csv');
+t.on('error', function (err) {
+	// 'err' contains error object
+  console.log('ERROR!! ' + err);
+});
+
+t.on('line', function (line) {
+	// 'line' contains the current line without the trailing newline character.
+  if(line != null && line != ''){
+    tweets.push(line);
+  }
+  //console.log('Line read!');
+});
+
+t.on('end', function () {
+  console.log(tweets);
+});
+
 var lr = new LineByLineReader('Keywords.csv'), tracks = [];
 lr.on('error', function (err) {
 	// 'err' contains error object
@@ -27,23 +53,26 @@ lr.on('line', function (line) {
 lr.on('end', function () {
 	// All lines are read, file is closed now.
   //Las Vegas bounding box SW/NE Long/Lat pair
+  console.log(tracks);
   tracks = tracks.toString();
-  tracks = tracks.replace(',',' OR ');
+  tracks = tracks.replace(/,/gi , ' OR ');
   console.log(tracks);
   console.log('Tracks loaded!');
 
-  T.get('search/tweets', { q: tracks, count: 100, language: 'en', locations:locations }, function(err, data, response) {
+  T.get('search/tweets', { q: tracks, count: 200, language: 'en', locations:locations, since: '2016-04-01' }, function(err, data, response) {
     //data.statuses
     for(var i = 0; i < data.statuses.length; i++){
-      console.log(data.statuses[i].id+') '+data.statuses[i].text);
-      //console.log(data.statuses[i].user.screen_name);
-      T.post('favorites/create', {id: data.statuses[i].id }, function(err, data, response) {
+      //console.log(data.statuses[i]);
+      var rand = Math.floor((Math.random() * tweets.length));
+      console.log('@'+ data.statuses[i].user.screen_name +tweets[rand]);
+      /*T.post('favorites/create', {id: data.statuses[i].id }, function(err, data, response) {
         console.log(data)
       })
 
-      T.post('statuses/update', { status: '@'+ data.statuses[i].user.screen_name +process.env.TWEET, in_reply_to_status_id: data.statuses[i].id }, function(err, data, response) {
-        //console.log(data)
-      })
+      T.post('statuses/update', { status: '@'+ data.statuses[i].user.screen_name +tweets[rand], in_reply_to_status_id: data.statuses[i].id }, function(err, data, response) {
+        console.log(data)
+      })*/
+
     }
   })
 
--- stream.js
+++ stream.js
@@ -7,7 +7,14 @@ var T = new Twit({
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
-
+var tweets = [
+  ' Please, check out my video and R/T to people who want to save the kids!! https://youtu.be/XoKqCQDNmmY #Freshwadda',
+  ' Trying to reach all of those who care about our next generation,  R/T and share please: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' Music Video response to the presidential campaign, please R/T: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' My music video "For The People!!"!! Please R/T: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' My music video "WARNING", a message to the kids: https://youtu.be/XoKqCQDNmmY  #Freshwadda',
+  ' Please review & R/T my music video talking about lessons we must pass on: https://youtu.be/XoKqCQDNmmY  #Freshwadda'
+]
 var locations = [['-115.414625','36.129623','-115.062072','36.380623'],['36.49','-89.57','39.15','-81.97']];
 var LineByLineReader = require('line-by-line');
 var lr = new LineByLineReader('Keywords.csv'), tracks = [];
@@ -34,14 +41,15 @@ lr.on('end', function () {
 
   stream.on('tweet', function (tweet) {
     console.log(tweet)
-    /*T.post('statuses/update', { status: '@'+ tweet.user.screen_name +', Please review and R/T my music video about our times: https://youtu.be/XoKqCQDNmmY  #FreshwaddaBrooks', in_reply_to_status_id: tweet.id }, function(err, data, response) {
+    var rand = Math.floor((Math.random() * 6));
+    T.post('statuses/update', { status: '@'+ tweet.user.screen_name +tweets[rand], in_reply_to_status_id: tweet.id }, function(err, data, response) {
       console.log(data)
-    })*/
+    })
   })
-/*
+
   stream.on('follow', function(event){
     console.log('You were followed');
     T.post('direct_messages/new',{user_id: event.target.id, text: process.env.DM });
-  });*/
+  });
 
 });
